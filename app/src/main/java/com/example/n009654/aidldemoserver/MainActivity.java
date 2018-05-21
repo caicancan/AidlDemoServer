b9e4482dbba486f5ceb500c025ee2763d55e70b3
caicancan <18062411472@163.com>
2018-05-21 18:02:03 +0800

    学习android5.0新特性

--- app/src/main/java/com/example/n009654/aidldemoserver/MainActivity.java
+++ app/src/main/java/com/example/n009654/aidldemoserver/MainActivity.java
@@ -3,28 +3,59 @@ package com.example.n009654.aidldemoserver;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.AudioTrack;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import com.ffpy.demo.IMyAidlInterface;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.n009654.aidldemoserver.BlankFragment.OnFragmentInteractionListener;
+import com.ffpy.demo.IMyAidlInterface;
+import com.squareup.leakcanary.RefWatcher;
+
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity implements  OnFragmentInteractionListener {
 
     private TextView tv;
+    private Button jupe;
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Toast.makeText(MainActivity.this,"this is："+uri,Toast.LENGTH_SHORT).show();
+    }
+
+    class LeakThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(6 * 60 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LeakThread leakThread = new LeakThread();
+        leakThread.start();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intent = new Intent();
@@ -49,7 +80,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        replace();
+        //初始化数据存储
+        HashSet<String> set = new HashSet<>();
+        set.add("11");
+        set.add("25");
+        set.add("38");
+        set.add("42");
+        set.add("38");
+        Log.i("ccc",""+set);
+        if (set.contains("42")){
+            Toast.makeText(getApplicationContext(),"哈哈",Toast.LENGTH_SHORT).show();
+        }
+        jupe = (Button) findViewById(R.id.judp);
+        jupe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //replace();
+                Intent intent1 = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent1);
+                // showContent();
+            }
+        });
+
     }
+
+    private void showContent() {
+        BlankFragment fragment1 = (BlankFragment)getSupportFragmentManager().findFragmentById(R.id.fv_content);
+        if (fragment1==null){
+            Toast.makeText(getApplicationContext(),"null",Toast.LENGTH_SHORT).show();
+        }else {
+            fragment1.show("ccccccccccccccccc");
+        }
+    }
+
+    private void replace() {
+        BlankFragment fragment = new BlankFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fv_content, fragment,"ccc");
+        fragmentTransaction.commit();
+    }
+
     IMyAidlInterface mStub;
     private ServiceConnection serviceConnection = new ServiceConnection() {
            @Override
@@ -78,6 +149,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         unbindService(serviceConnection);
+        RefWatcher refWatcher = LeakApplication.getRefWatcher(this);//1
+        refWatcher.watch(this);
     }
 
     @Override
---
+++ app/src/main/java/com/example/n009654/aidldemoserver/SecondActivity.java
@@ -0,0 +1,57 @@
+package com.example.n009654.aidldemoserver;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initView();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+    }
+
+    private void initView() {
+        TabLayout tabs = (TabLayout) findViewById(R.id.tablayout);
+        tabs.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
+        tabs.addTab(tabs.newTab().setText("Tab1"));
+        tabs.addTab(tabs.newTab().setText("Tab2"));
+        tabs.addTab(tabs.newTab().setText("Tab3"));
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+}
