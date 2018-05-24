634f9e06ac930bca61b3abd927450d4f2afa17ab
caicancan <18062411472@163.com>
2018-05-24 14:22:11 +0800

    添加关闭所以的activity

--- app/src/main/java/com/example/n009654/aidldemoserver/LeakApplication.java
+++ app/src/main/java/com/example/n009654/aidldemoserver/LeakApplication.java
@@ -2,9 +2,13 @@ package com.example.n009654.aidldemoserver;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeakApplication extends Application {
     private RefWatcher refWatcher;
@@ -18,6 +22,21 @@ public class LeakApplication extends Application {
             return RefWatcher.DISABLED;
         }
         return LeakCanary.install(this);
+    }
+    private static List<AppCompatActivity> lists = new ArrayList<>();
+
+    public static void addActivity(AppCompatActivity activity) {
+        lists.add(activity);
+    }
+
+    public static void clearActivity() {
+        if (lists != null) {
+            for (AppCompatActivity activity : lists) {
+                activity.finish();
+            }
+
+            lists.clear();
+        }
     }
 
     public static RefWatcher getRefWatcher(Context context) {
